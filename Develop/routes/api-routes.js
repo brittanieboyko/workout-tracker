8da7c05743d86e2e75f61271bff4905a88eb9919
brittanieb <brittanie.boyko@gmail.com>
2020-03-28 18:47:45 -0400

    created put route in api-routes to add a new workout to the db

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -3,11 +3,44 @@ const db = require("../models");
 module.exports = function(app) {
     app.get("/api/workouts", (req, res) => {
         db.Workout.find({})
-        .then(dbWorkout => {
-          res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    });
+
+    app.post("/api/workouts", ({
+        body
+    }, res) => {
+        db.Workout.create(body)
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    });
+
+    app.put("/api/workouts/:id", ({
+        body
+    }, res) => {
+        db.Workout.create(body)
+            .then(({
+                _id
+            }) => db.Workout.findOneAndUpdate({}, {
+                $push: {
+                    exercises: _id
+                }
+            }, {
+                new: true
+            }))
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
     });
 };
